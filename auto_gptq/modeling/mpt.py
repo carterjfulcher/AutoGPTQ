b2dda177b1362d79d30b86684a2e804e2f2e9715
LaaZa <jesse.laatu@gmail.com>
2023-11-24 20:06:30 +0200

    Support for Custom MPT sealion models.

--- auto_gptq/modeling/mpt.py
+++ auto_gptq/modeling/mpt.py
@@ -5,7 +5,7 @@ class MPTGPTQForCausalLM(BaseGPTQForCausalLM):
     layer_type = "MPTBlock"
     layers_block_name = "transformer.blocks"
     outside_layer_modules = [
-        "transformer.wte",  "transformer.norm_f"
+        "transformer.wte", "transformer.wpe",  "transformer.norm_f"
     ]
 
     inside_layer_modules = [
@@ -14,5 +14,6 @@ class MPTGPTQForCausalLM(BaseGPTQForCausalLM):
         ["ffn.up_proj"],
         ["ffn.down_proj"]
     ]
+
 
 __all__ = ["MPTGPTQForCausalLM"]
